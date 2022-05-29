ed11c912e32f70285604986ad68ca67f9b3c6aff
r4u1 <raul.iturraldec@gmail.com>
2022-05-29 00:32:37 -0500

    Models
    Se crea clase para el modelo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+# Interactuar con la base de datos
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    # Contenido ilimitado
+    content = models.TextField()
