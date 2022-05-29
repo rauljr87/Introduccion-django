68663bc82cde1293e807c80578f977e757b35a79
r4u1 <raul.iturraldec@gmail.com>
2022-05-29 00:26:57 -0500

    blog_list view urls
    Se mapea urls

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+
 # Create your views here.
 # Indice del blog
 # Todos los post que existen
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
         context = {
-            
+
         }
-        return render(request, 'bolg_list.html', context)
+        return render(request, 'blog_list.html', context)
