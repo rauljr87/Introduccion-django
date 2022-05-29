af700e7fd03299241f916f1a8ce8a606fbe3a53c
r4u1 <raul.iturraldec@gmail.com>
2022-05-29 00:39:31 -0500

    blog/admin
    Registro de modelo en blog/admin

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Interactuar con la base de datos
 # Create your models here.
+# Cada vez que se hace un cambio en este archivo debemos generar una nueva migración
 class Post(models.Model):
     title = models.CharField(max_length=250)
     # Contenido ilimitado
