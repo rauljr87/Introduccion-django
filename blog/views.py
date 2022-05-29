5630d5acd52538d898272f9f5538e8176d4e91ad
r4u1 <raul.iturraldec@gmail.com>
2022-05-29 00:57:01 -0500

    Vista para formulario PostCreateForm
    Se crea la vista BlogCreateView

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from .forms import PostCreateForm
 
 
 # Create your views here.
 # Indice del blog
 # Todos los post que existen
 class BlogListView(View):
+    """ Genera página para blog_list """
+
     def get(self, request, *args, **kwargs):
         context = {
 
         }
         return render(request, 'blog_list.html', context)
+
+
+class BlogCreateView(View):
+    """ Vista para formulario PostCreateForm """
+
+    def get(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, 'blog_create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, 'blog_create.html', context)
