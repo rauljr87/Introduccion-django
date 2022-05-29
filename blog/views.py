f4a68487147f9366e6ca0785ba71926a1ba1ede6
r4u1 <raul.iturraldec@gmail.com>
2022-05-29 01:39:08 -0500

    Vista para formulario
    - Se crea vista para formulario Post
    - Se crea archivo HTML blog_create

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.base import View
 from .forms import PostCreateForm
+# Para crear Post declarado en el modelo
+from .models import Post
 
 
-# Create your views here.
 # Indice del blog
 # Todos los post que existen
 class BlogListView(View):
-    """ Genera página para blog_list """
+    """ Vista para blog_list """
 
     def get(self, request, *args, **kwargs):
         context = {
@@ -17,15 +18,34 @@ class BlogListView(View):
 
 
 class BlogCreateView(View):
-    """ Vista para formulario PostCreateForm """
+    """ Vista para crear formulario PostCreateForm """
 
+    # Solicitando información
     def get(self, request, *args, **kwargs):
+        form = PostCreateForm()
         context = {
-
+            'form':form
         }
         return render(request, 'blog_create.html', context)
 
+    # Enviando información
     def post(self, request, *args, **kwargs):
+        # Si el método es POST
+        if request.method=="POST":
+            # Pedir el formulario
+            form = PostCreateForm(request.POST)
+            # Si el formulario es válido
+            if form.is_valid():
+                # Obtener
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+
+                # Crear POST
+                p, created = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect('blog:home')
+
+
         context = {
 
         }
